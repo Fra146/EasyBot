227d3218c3a8ad9e6385581d1ea78d8b6cf4ccc6
Fra146 <francescocammarata.fr@gmail.com>
2020-12-11 11:18:27 +0100

    Created Repo and added files

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord, json
+
+def die(reason):
+    print(reason)
+    exit()
+
+def error(reason):
+    if reason == "json":
+        die("Error in json file")
+
+f = open("commands.json", "r")
+not_serial = f.read()
+f.close()
+com_list = json.loads(not_serial)
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Executed login with {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content in com_list['Commands']:
+        await message.channel.send(com_list['Commands'][message.content])
+    else:
+        await message.channel.send(com_list['Config']['NotFoundError'])
+f = open("token.txt", "r")
+token = f.read()
+f.close()
+
+client.run(token)
